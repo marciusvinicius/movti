51c2baf3b72ad30d0f8bd53fd37a72e8b3f526a8
Marcius Oliveira <marciusoliveiraa@gmail.com>
2017-06-05 17:30:51 -0300

    Check the blank field for Content

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+from datetime import datetime
+from django.conf import settings
 from django.db import models
 
 
@@ -10,7 +13,8 @@ class Photo(models.Model):
     """
     name = models.CharField(max_length=255)
     description = models.TextField(null=True)
-    content = models.TextField(null=True)
+    content = models.TextField(null=True, blank=True)
+    upload = models.ImageField()
 
     def __str__(self):
         return self.name

79b868b69e8d1f07d08d58406948d1a1fc10e99e
Marcius Oliveira <marciusoliveiraa@gmail.com>
2017-06-05 12:44:03 -0300

    add google handle and google custom storage

---
+++ google_handle/storage/google_cloud.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import os
+import mimetypes
+
+from django.conf import settings
+from django.core.files.base import ContentFile
+from django.core.files.storage import Storage
+
+from google.appengine.ext.blobstore import create_gs_key
+import cloudstorage as gcs
+
+
+class GoogleCloudStorage(Storage):
+    """
+    Creating a Django Storage for google cloud
+    """
+    def __init__(self, location=None, base_url=None):
+        if location is None:
+            location = settings.GOOGLE_CLOUD_STORAGE_BUCKET
+        self.location = location
+        if base_url is None:
+            base_url = settings.GOOGLE_CLOUD_STORAGE_URL
+        self.base_url = base_url
+
+    def _open(self, name, file_mode='r'):
+        filename = "%s/%s" % (self.location, name)
+        gcs_file = gcs.open(filename, mode=file_mode)
+        content_file = ContentFile(gcs_file.read())
+        gcs_file.close()
+        return content_file
+
+    def _save(self, name, content):
+        filename = "%s/%s" % (self.location, name)
+        filename = os.path.normpath(filename)
+        file_type, _ = mimetypes.guess_type(name)
+        gss_file = gcs.open(
+            filename, mode='w', content_type=file_type,
+            options={
+                'x-goog-acl': 'public-read',
+                'cache-control': settings.GOOGLE_CLOUD_STORAGE_DEFAULT_CACHE_CONTROL
+            }
+        )
+        content.open()
+        gss_file.write(content.read())
+        content.close()
+        gss_file.close()
+        return name
+
+    def size(self, name):
+        """
+        Return the size
+        """
+        stats = self.stat_file(name)
+        return stats.st_size
+
+    def url(self, name):
+        """
+        Return the URL Based on debug
+        """
+        if settings.DEBUG:
+            filename = "/gs%(location)s/%(name)s"  % {
+                "location": self.location,
+                "name": name,
+            }
+            key = create_gs_key(filename)
+            return "http://localhost:8000/blobstore/blob/"+key+"?display=inline"
+        return "%s/%s" % (self.base_url, name)
+
+    def stat_file(self, name):
+        """
+        Return the Stat
+        """
+        filename = "%s/%s" % (self.location, name)
+        return gcs.stat(filename)
---
+++ google_handle/test/test_handle.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from django.conf import settings
+from google_handle.storage.google_cloud import GoogleCloudStorage
+
+import mock
+
+
+class GoogleStorageTestCase(TestCase):
+
+    def test_create_init(self):
+        storage = GoogleCloudStorage()
+        self.asertEqual(storage.location, settings.GOOGLE_CLOUD_STORAGE_BUCKET)
+        self.asertEqual(storage.base_url, settings.GOOGLE_CLOUD_STORAGE_URL)
+
+    @mock.path("google_handle.storage.google_clound.ContentFile")
+    @mock.path("google_handle.storage.google_clound.gcs")
+    def test_on_open(self, gcs, ContentFile):
+        storage = GoogleCloudStorage()
+        storage.open("test.png")
+        file_name = "%s/%s" % (
+            settings.GOOGLE_CLOUD_STORAGE_URL,
+            "test.png")
+        gcs.open.assert_called_with(file_name, "r")
+        gcs_file = mock.Mock()
+        gcs.open.return_value = gcs_file
+        gcs_file.read.assert_called()
+
+    @mock.path("google_handle.storage.google_clound.gcs")
+    def test_on_save(self, gcs):
+        storage = GoogleCloudStorage()
+        content = mock.Mock()
+        storage.save(name="marcius.png", content=content)
+        file_name = ""
+        gcs.open.assert_called_with(
+            file_name, mode='w', content_type="png",
+            options={
+                'x-goog-acl': 'public-read',
+                'cache-control': settings.GOOGLE_CLOUD_STORAGE_DEFAULT_CACHE_CONTROL
+            })
+
+        gss_file = mock.Mock()
+        gcs.open.return_value = gss_file
+        gss_file.write.assert_called_with(content.read())
+        content.close.assert_called()
+        gss_file.close.assert_called()
